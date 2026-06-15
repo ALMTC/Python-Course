4a3b36c3d0f31908058c99b3e5260068dad384a2
ALMTC <antonilucasmateus@gmail.com>
2026-06-15 14:59:34 -0300

    Adicionar funcionalidades de salvar e carregar tarefas em formato JSON, além de ajustes nas funções de adicionar, desfazer e refazer tarefas

--- Aula194.py
+++ Aula194.py
@@ -1,51 +1,69 @@
+import json
 import os
 import copy
 
+lista_de_tarefas = []
+lista_de_tarefas_maxima = []
+
 def exibir_lista(lista_de_tarefas):
-    os.system("cls")
+    #limpar_tela()
     print("Tarefas:\n")
     for tarefa in lista_de_tarefas:
         print(tarefa)
     print("\n")
 
 
-def adicionar_tarefa(tarefa, lista_de_tarefas, lista_de_tarefas_maxima):
+def adicionar_tarefa(tarefa):
     if len(tarefa) > 0:
         lista_de_tarefas.append(tarefa)
+        global lista_de_tarefas_maxima
         lista_de_tarefas_maxima = copy.deepcopy(lista_de_tarefas)
     exibir_lista(lista_de_tarefas)
-    exibir_lista(lista_de_tarefas_maxima)
 
-def desfazer(lista_de_tarefas):
+def desfazer():
     if len(lista_de_tarefas) > 0:
         lista_de_tarefas.pop()
     exibir_lista(lista_de_tarefas)
 
-def refazer(lista_de_tarefas, lista_de_tarefas_maxima):
+def refazer():
     tamanho_lista_de_tarefas = len(lista_de_tarefas)
     if tamanho_lista_de_tarefas < len(lista_de_tarefas_maxima):
     
         lista_de_tarefas.append(lista_de_tarefas_maxima[tamanho_lista_de_tarefas])
     exibir_lista(lista_de_tarefas)
 
+def limpar_tela():
+    os.system("cls")
+
+def salvar(lista_de_tarefas):
+    with open('aula194.json', 'w+',encoding="utf-8") as arquivo:
+        json.dump(lista_de_tarefas, arquivo, ensure_ascii=False, indent=2)
+
+def carregar():
+    global lista_de_tarefas
+    try:
+        with open('aula194.json', 'r', encoding="utf-8") as arquivo:
+            lista_de_tarefas = json.load(arquivo)
+        exibir_lista(lista_de_tarefas)
+    except FileNotFoundError:
+        print("Nenhum arquivo de tarefas encontrado")
+        with open('aula194.json', 'w+',encoding="utf-8") as arquivo:
+            json.dump(lista_de_tarefas, arquivo, ensure_ascii=False, indent=2)
 
 verifica = True
 
-lista_de_tarefas = []
-lista_de_tarefas_maxima = []
-
 while verifica:
-    print("Comandos: listar, desfazer, refazer, sair")
+    print("Comandos: listar, desfazer, refazer, salvar, sair")
     comando = input("Digite um comando: ").lower()
 
     if comando == "listar":
         exibir_lista(lista_de_tarefas)
 
     elif comando == "desfazer":
-        desfazer(lista_de_tarefas)
+        desfazer()
 
     elif comando == "refazer":
-        refazer(lista_de_tarefas, lista_de_tarefas_maxima)
+        refazer()
 
     elif comando == "sair":
         verifica = False
@@ -53,5 +71,11 @@ while verifica:
     elif comando == "limpar":
         os.system("cls")
 
+    elif comando == "salvar":
+        salvar(lista_de_tarefas)
+
+    elif comando == "carregar":
+        carregar()
+        
     else:
-        adicionar_tarefa(comando, lista_de_tarefas, lista_de_tarefas_maxima)
+        adicionar_tarefa(comando)
